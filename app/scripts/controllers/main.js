dafd90c70556998aabd35c7ca5bbe6004c5a98c6
Julian Poyourow <julianpoyo@gmail.com>
2014-06-17 15:45:10 -0700

    Gen Angular Files

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name treeMdbApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the treeMdbApp
+ */
+angular.module('treeMdbApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
