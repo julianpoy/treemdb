40f215346abf9e60a9f991f16d8b1183e1b5743c
Julian Poyourow <julianpoyo@gmail.com>
2014-06-24 15:31:39 -0700

    Added New Contact page. Fixed header and footer.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -40,9 +40,9 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
+      .when('/add', {
+        templateUrl: 'views/newcontact.html',
+        controller: 'NewcontactCtrl'
       })
       .otherwise({
         redirectTo: '/'
