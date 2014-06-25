fe1fc4118d03a8ca5a9331e0b3a177910c9823ee
Julian Poyourow <julianpoyo@gmail.com>
2014-06-24 19:29:40 -0700

    Added search and display for query of database to main.html and its controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,36 @@
  * Controller of the treeMdbApp
  */
 angular.module('treeMdbApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, Contact, API_BASE) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    var postData = "2";
+    //$scope.contacts = Contact.get({Id: 2});
+    $scope.go = function(requrl){
+        console.log("url switch for " + requrl);
+    }
   });
+
+function ContactsDisplayCtrl($scope, Contact, API_BASE){
+        $scope.contacts = Contact.query();
+        $scope.sort = {
+            column: '',
+            descending: false
+        };    
+        $scope.limitquantity = 30;
+        $scope.changeSorting = function(column) {
+
+            var sort = $scope.sort;
+ 
+            if (sort.column == column) {
+                sort.descending = !sort.descending;
+            } else {
+                sort.column = column;
+                sort.descending = false;
+            }
+        };
+        
+}
